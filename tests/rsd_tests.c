a4e866a6c8b043e545c8e06f817be19cc024807e
Oleh Kulykov <oleh.kulykov@stock3.com>
2024-12-04 13:41:38 +0100

    1733316098

---
+++ tests/rsd_tests.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) Oleh Kulykov <olehkulykov@gmail.com>. All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ * Created by Oleh Kulykov <olehkulykov@gmail.com>, 2024.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <unistd.h>
+
+#include "../rsd_pid.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    for (int i = 0; i < 10; i++) {
+        fprintf(stdout, "%i ", i);
+        fflush(stdout);
+        usleep(100);
+    }
+    int res = 0;
+//    std::cout << "Hello, World!\n";
+    return res;
+}
